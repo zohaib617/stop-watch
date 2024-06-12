c3799bbef80d3ac5f7304a3882cdd3036fd7c47f
zohaib617 <160512319+zohaib617@users.noreply.github.com>
2024-06-12 12:40:49 -0700

    Add files via upload
    stop watch

---
+++ index.ts
@@ -0,0 +1,41 @@
+#! /usr/bin/env node
+import inquirer from "inquirer"
+import {differenceInSeconds} from "date-fns"
+
+
+const res = await inquirer.prompt({
+    name:"userinput",
+    type:"number",
+    message:"please enter the amount of second",
+    validate:(input)=>{
+        if(isNaN(input)){
+            return "please enter valid number"
+        }else if (input > 60){
+            return "seconsd must be in 60"
+        }else {
+            return true;
+        }
+    }
+})
+let input = res.userinput
+ function starttime(val:number){
+    const intTime = new Date().setSeconds(new Date().getSeconds() + val);
+    const intervalTime = new Date(intTime)
+    setInterval((()=>{
+        const currTime = new Date()
+        const timeDiff = differenceInSeconds(intervalTime , currTime);
+            if (timeDiff <=0 ){
+                console.log("time has expired")
+                process.exit();
+            }
+            
+            const min = Math.floor((timeDiff%(3600*24))/3600);
+            const sec = Math.floor(timeDiff%60);
+            console.log(`${min.toString().padStart(2, "0")} ${sec.toString().padStart(2, "0")}`);
+            
+            
+
+            
+    }),1000)
+ }
+ starttime(input)
